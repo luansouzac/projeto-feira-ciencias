6f4c576009dacef77e2811d2cffad966059cc4c7
luansouzac <luansouzac14@gmail.com>
2025-06-19 10:26:13 -0300

    model tipousario e usuario

--- apiFeira/app/Models/TipoUsuario.php
+++ apiFeira/app/Models/TipoUsuario.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class TipoUsuario extends Model
 {
     use HasFactory; //preencher model
+
+    protected $table = 'tipo_usuarios';
+
+    protected $fillable = ['id_tipo_usuario', 'tipo'];
+
+    public function id_tipo_usuario()
+    {
+       return $this->hasMany(Usuario::class); //tipo usuario esta em N Usuarios
+    }
 }
--- apiFeira/app/Models/Usuario.php
+++ apiFeira/app/Models/Usuario.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+
+    protected $table = 'usuarios';
+
+    protected $fillable = ['id_usuario', 'nome', 'email', 'senha_hash', 'id_tipo_usuario'];
+
+    public function id_tipo_usuario()
+    {
+        return $this->belongsTo(TipoUsuario::class); //coluna tipo usario PERTENCE a classe TipoUsuario
+    }
 }
