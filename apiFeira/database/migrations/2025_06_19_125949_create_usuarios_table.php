8a9137c144351f42688ccf5840943b5b39e127e1
luansouzac <luansouzac14@gmail.com>
2025-06-19 10:08:12 -0300

    primeiro commit

---
+++ apiFeira/database/migrations/2025_06_19_125949_create_usuarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('id_usuario');
+            $table->string('nome', 100);
+            $table->string('email', 100);
+            $table->string('senha_hash', 255);
+            $table->unsignedBigInteger('id_tipo_usuario');
+            $table->foreign('id_tipo_usuario')->references('id_tipo_usuario')->on('tipo_usuarios')->onDelete('no action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
