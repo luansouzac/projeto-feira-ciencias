0d2bb1fe4cb425cf00a66f1e70d6748ca01317a8
diegorossi0 <diego.rossi@yahoo.com.br>
2025-07-09 14:36:52 -0300

    feat: Adicionando o Axios e o Vuetify no front

---
+++ feira_ciencias/plugins/axios.js
@@ -0,0 +1,5 @@
+//Instalar o axios npm install axios
+import axios from 'axios';
+
+//Caminho raiz da API definido no .env
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
--- feira_ciencias/src/main.js
+++ feira_ciencias/src/main.js
@@ -1,5 +1,8 @@
 import './assets/main.css'
 
+//axios
+import '@/plugins/axios'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -10,5 +13,15 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+//vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify/lib/framework.mjs'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({components, directives})
+app.use(vuetify);
 
 app.mount('#app')
