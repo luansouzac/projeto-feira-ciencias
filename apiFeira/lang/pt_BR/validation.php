0d2bbc28a38fd79ce67d1c6a6d15aab76ec76468
luansouzac <luansouzac14@gmail.com>
2025-07-09 21:34:27 -0300

    feat: Adicionar mensagem de sucesso ao cadastrar

--- apiFeira/lang/pt_BR/validation.php
+++ apiFeira/lang/pt_BR/validation.php
@@ -100,11 +100,12 @@ return [
     'mimes' => 'The :attribute field must be a file of type: :values.',
     'mimetypes' => 'The :attribute field must be a file of type: :values.',
     'min' => [
-        'array' => 'The :attribute field must have at least :min items.',
-        'file' => 'The :attribute field must be at least :min kilobytes.',
-        'numeric' => 'The :attribute field must be at least :min.',
-        'string' => 'The :attribute field must be at least :min characters.',
+        'array' => 'O campo :attribute deve ter pelo menos :min itens.',
+        'file' => 'O campo :attribute deve ter no mínimo :min kilobytes.',
+        'numeric' => 'O campo :attribute deve ser no mínimo :min.',
+        'string' => 'O campo :attribute deve ter no mínimo :min caracteres.',
     ],
+
     'min_digits' => 'The :attribute field must have at least :min digits.',
     'missing' => 'The :attribute field must be missing.',
     'missing_if' => 'The :attribute field must be missing when :other is :value.',
