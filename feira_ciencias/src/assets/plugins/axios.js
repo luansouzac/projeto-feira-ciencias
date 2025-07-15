5c7b66c76bca3a45e0d88386bd03f0667e64851d
luansouzac <luansouzac14@gmail.com>
2025-07-14 23:26:31 -0300

    fix: Conserta redirecionamento para o login após expirar a sessao

--- feira_ciencias/src/assets/plugins/axios.js
+++ feira_ciencias/src/assets/plugins/axios.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
-import { useRouter } from 'vue-router';
 import { useNotificationStore } from '@/stores/notification';
 
-const router = useRouter();
-
 const api = axios.create({
-
   baseURL: import.meta.env.VITE_API_URL,
-
   timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
@@ -18,41 +13,39 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const userDataString = localStorage.getItem('user_data');
-
     if (userDataString) {
       const userData = JSON.parse(userDataString);
       const token = userData.token;
-
       if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
       }
     }
-    
     return config;
   },
   (error) => {
     return Promise.reject(error);
   }
 );
+
 api.interceptors.response.use(
   (response) => response,
-
-  (error) => {
+  async (error) => { 
     const notificationStore = useNotificationStore();
+
     if (error.code === 'ECONNABORTED' || error.message.includes('timeout')) {
       notificationStore.showError('O servidor demorou muito para responder. Por favor, tente novamente.');
-    }
-    else if (!error.response) {
+    } else if (!error.response) {
       notificationStore.showError('Não foi possível conectar ao servidor. Verifique sua conexão com a internet.');
-    }
-    else if (error.response.status === 401) {
+    } else if (error.response.status === 401) {
       notificationStore.showError('Sua sessão expirou. Por favor, faça login novamente.');
+    
+      const router = (await import('@/router')).default;
       router.push('/login');
-    }
-    else {
+
+    } else {
       notificationStore.showError('Ocorreu um erro inesperado.');
     }
-
+    
     return Promise.reject(error);
   }
 );
