c00bbe777b6a821fd40353bd37c5f686440f0f7c
diegorossi0 <diego.rossi@yahoo.com.br>
2025-09-17 16:47:44 -0300

    fix: adicionando o id_matricula no alter table do usuário

--- apiFeira/database/migrations/2025_09_15_221255_add_campos_perfil_to_usuarios_table.php
+++ apiFeira/database/migrations/2025_09_15_221255_add_campos_perfil_to_usuarios_table.php
@@ -8,6 +8,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('usuarios', function (Blueprint $table) {
+            $table->string('id_matricula', 100)->after('id_tipo_usuario');
             $table->string('telefone', 20)->nullable()->after('id_matricula');
             $table->string('ano', 50)->nullable()->after('telefone');
             $table->string('photo')->nullable()->after('ano'); // pode guardar o path da imagem
