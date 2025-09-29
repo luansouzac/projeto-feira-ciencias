5c1ec0a8c8cc3d6f733cf98a2e86ff0d661d3479
diegorossi0 <diego.rossi@yahoo.com.br>
2025-09-29 16:50:12 -0300

    feat: Criando o template do email de recuperação de senha

---
+++ apiFeira/resources/views/emails/temporary_password.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sua Nova Senha Temporária</title>
+</head>
+<body>
+    <h1>Redefinição de Senha Solicitada</h1>
+    
+    <p>Olá,</p>
+    
+    <p>Sua nova senha temporária é:</p>
+    
+    <h2 style="color: #d9534f;">{{ $password }}</h2>
+    
+    <p>Por questões de segurança, recomendamos que você faça login e altere sua senha imediatamente.</p>
+    
+    <p>Se você não solicitou esta alteração, por favor, ignore este e-mail.</p>
+    
+    <p>Atenciosamente,<br>Sua Equipe de Suporte</p>
+</body>
+</html>
